321e48ec2b308bd861b9abb5a62e57ed2c7cc39c
Muskan Agnihotri <muskan21agni@gmail.com>
2026-07-10 22:27:08 +0530

    delete main

--- main.py
+++
@@ -1,4 +0,0 @@
-# Agroforestry Project
-# Starting point for the Agroforestry Project application.
-# This script initializes the application, sets up the necessary configurations, and starts the main event loop.
-# new 
